71c3ff827bf43df1ca0b7d5c6b2cfd9ad0bcc7e7
islam almadani <islamsabri2005@gmail.com>
2022-01-14 22:01:39 +0200

    Changes on the style and the Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,17 @@ function Card(props) {
 
     return (
         <div>
-            <div className="App">
+            <div>
                 <h1 className="details_header">{location.state.name}</h1>
+                <div className="details_div">
+               
                 <img src={location.state.url} className="details_image" alt='img' />
-
-                <ul id="li">
-                    <li className="details_li"> Location : {location.state.country}</li>
-                    <li className="details_li"> description : {location.state.description}</li>
-                </ul>
-                <a href={location.state.web_pages[0]} target="_blank" className='details_link' rel="noreferrer" >University web page</a>
+                </div>
+                <h2 className="details_h2">The Location :</h2>
+                <p className="details_par">{location.state.country}</p>
+                <h2 className="details_h2">Description :</h2>
+                <p className="details_par">{location.state.description}</p>
+                <a href={location.state.web_pages[0]} target="_blank" className='details_link' id="details_link" rel="noreferrer" >University web page</a>
 
             </div>
         </div>
